37d79b4c7ca881bfce706b923aa8a31b8b19cd1c
KDNagpl <Karandeepnagpal2@gmail.com>
2023-05-02 08:23:05 -0400

    make it better

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const { traceDeprecation } = require('process');
 const {User, Post, Comment} = require('../../models')
+const authUser = require('../../utils/auth');
 
 router.post('/newpost', async(req,res) => {
     try{
@@ -12,13 +14,31 @@ router.post('/newpost', async(req,res) => {
     }
 });
 
-router.post('/newcomment', async(req,res) => {
-    try{ const {body, postId} = req.body
-    const postData = await Comment.create({comment_body:body, post_id:postId, user_id:res.session.user.id})
-    if (!postData) return res.status(400).json('Comment not posted!')
-    res.status(200).json('Comment posted!')
+router.post('/newcomment', async(req, res) => {
+    try{
+    const {body, postId} = req.body
+
+    const postData = await Comment.create({comment_body:body, post_id: postId, user_id:req.session.user.id})
+    if (!postData) return res.status(400).json("Comment not created!")
+
+    res.status(200).json("Comment created!")
+
     } catch (err){res.status(500).json(err)}
 })
 
+router.put('/editpost', async(req,res) => {
+    try { const {body,id} = req.body
+        const postData = await Post.update({post_body:body}, {where:{id}})
+    if (!postData)return res.status(400).json('Post not updated!')
+    res.status(200).json('Post updated!')}
+    catch(err){res.status(500).json(err)}
+})
+router.delete('/deletepost', async(req,res) => {
+    try { const {id} = req.body
+        const postData = await Post.destroy({where:{id}})
+    if (!postData)return res.status(400).json('Unable to delete post!')
+    res.status(200).json('Post deleted!')}
+    catch(err){res.status(500).json(err)}
+})
 
 module.exports = router;
--- controllers/home.js
+++ controllers/home.js
@@ -3,7 +3,7 @@ const { Post, User } = require('../models');
 const router = require('express').Router();
 
 router.get('/', async(req, res) => {
-    const posts = await Post.findAll({include:User})
+    const posts = await Post.findAll({include:User,  order: [["createdAt", "DESC"]]})
     const blogPosts = posts.map(post => post.get({plain:true}))
     res.render('homepage', {blogPosts, loggedIn:req.session.loggedIn, user:req.session.user})
 })
